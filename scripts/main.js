18910a9fdb0b34242b182c8b8392760aceb756bf
Ramires <moreira.ramires@gmail.com>
2022-06-01 16:09:35 -0300

    resolve first chalange with class 02

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 function tocaSomPom(){
   document.querySelector('#som_tecla_pom').play();
+
+  return 1
+}
+
+function tocaSomClap(){
+  document.querySelector('#som_tecla_clap').play();
+
+  return 1
 }
 
 
