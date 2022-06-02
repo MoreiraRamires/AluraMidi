010b9c377ce36fa43dd3d293be56e77686d87633
Ramires <moreira.ramires@gmail.com>
2022-06-02 16:36:55 -0300

    use lists in my code

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,6 @@ function tocaSomPom(){
   document.querySelector('#som_tecla_pom').play();
 
 }
-const listaTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick=tocaSomPom 
