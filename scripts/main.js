da433e0e85ef579463bb2bd7476defc21966b474
Ramires <moreira.ramires@gmail.com>
2022-06-03 12:17:10 -0300

    teclas emitindo os sons de forma correta

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,20 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0 ;
 
 //enquanto 
+
+
+
 while (contador < listaDeTeclas.length) {
   //loop => rotina  
-  listaDeTeclas[contador].onclick=tocaSom;
+  const tecla = listaDeTeclas[contador];
+  const teclaClasseCSS = tecla.classList[1];
+  const idAudio = `#som_${teclaClasseCSS}`;
+
+  console.log(teclaClasseCSS);
+
+  tecla.onclick= function (){
+    tocaSom(idAudio)
+  };
 
   contador= contador+1;
   console.log(contador);
